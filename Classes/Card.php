6049c0a87a1b18f98e5ce24ad3d04890d72f4f0f
LABAN <luumyyen@gmail.com>
2015-11-14 04:21:44 +0000

    1

---
+++ Classes/Card.php
@@ -0,0 +1,10 @@
+<?php
+    class Card {
+       $title;
+       $content;
+       $keywords[];
+       $relationship //array [key=>value]  key: others_keyword value: relation_name
+       $addInfo;
+       
+        
+    }
